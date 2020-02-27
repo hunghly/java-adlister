c1406cb9f1b2ad288f163e366d92ba20f59f855a
Hung Ly <hunghly@gmail.com>
2020-02-27 17:00:50 -0600

    finished sessions

--- src/main/java/LoginServlet.java
+++ src/main/java/LoginServlet.java
@@ -3,22 +3,32 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet(name = "LoginServlet", urlPatterns = "/login")
 public class LoginServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HttpSession session = request.getSession();
+        if (session.getAttribute("user") != null && ((boolean) session.getAttribute("user"))) {
+            response.sendRedirect("/profile");
+            return;
+        }
         request.getRequestDispatcher("/login.jsp").forward(request, response);
     }
 
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
         String username = request.getParameter("username");
         String password = request.getParameter("password");
-        boolean validAttempt = username.equals("admin") && password.equals("password");
+        HttpSession session = request.getSession();
 
+        boolean validAttempt = username.equals("admin") && password.equals("password");
         if (validAttempt) {
+            session.setAttribute("user", true);
+            session.setAttribute("username", username);
             response.sendRedirect("/profile");
         } else {
+            session.setAttribute("user", false);
             response.sendRedirect("/login");
         }
     }
---
+++ src/main/java/LogoutServlet.java
@@ -0,0 +1,16 @@
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebServlet("/logout")
+public class LogoutServlet extends HttpServlet {
+
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        request.getSession().setAttribute("user", false);
+        response.sendRedirect("/login");
+    }
+}
