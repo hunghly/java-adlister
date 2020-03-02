523b56bd650e2653a2169ae666f76e5cd76f6206
Hung Ly <hunghly@gmail.com>
2020-03-02 10:21:04 -0600

    worked on jdbc-exercise

--- src/main/java/MySQLAdsDao.java
+++ src/main/java/MySQLAdsDao.java
@@ -1,5 +1,4 @@
 import com.mysql.cj.jdbc.Driver;
-
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
@@ -22,13 +21,39 @@ public class MySQLAdsDao implements Ads {
         List<Ad> returnList = new ArrayList<>();
         Statement statement = this.connection.createStatement();
         ResultSet rs = statement.executeQuery("SELECT * FROM ads");
+        while (rs.next()) {
 
+        }
 
         return returnList;
     }
 
     @Override
-    public Long insert(Ad ad) {
-        return null;
+    public Long insert(Ad ad) throws SQLException {
+        Statement statement = this.connection.createStatement();
+        statement.executeUpdate(String.format("INSERT INTO ads (user_id, title, description) VALUES ('%d', '%s', '%s')", (int) ad.getUserId(), ad.getTitle(), ad.getDescription()), Statement.RETURN_GENERATED_KEYS);
+        ResultSet rs = statement.getGeneratedKeys();
+        rs.next();
+        return rs.getLong((1));
+    }
+
+    public void generateAds() throws SQLException {
+
+    }
+
+
+
+    public static void main(String[] args) throws SQLException {
+        Ads mySQLDao = DaoFactory.getAdsDao();
+        Ad ps4 = new Ad (1, "old PS4", "An old playstation 4 for sale.");
+        Ad xbox = new Ad (1, "old Xbox", "An old Xbox for sale.");
+        Ad car = new Ad (2, "2018 toyota corolla", "near mint condition vehicle!");
+        Ad backpack = new Ad (2, "army backpack", "selling for cheap!.");
+//        Ads mySQLDao = DaoFactory.getAdsDao();
+        System.out.println(mySQLDao.insert(ps4));
+        mySQLDao.insert(xbox);
+        mySQLDao.insert(car);
+        mySQLDao.insert(backpack);
+        mySQLDao.all();
     }
 }
